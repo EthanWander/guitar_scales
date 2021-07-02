4ddc6f6a425cef5dcd834fd67a9db3730ef64549
EthanWander <ethanwander@gmail.com>
2021-07-02 22:07:17 +0300

    added print

--- main.py
+++ main.py
@@ -9,12 +9,31 @@ class Scale:
         self.scale = scale
         self.tuning = tuning
         self.root = root
+        self.notes_in_scales = self.get_notes_in_scale()
 
     def get_notes_in_scale(self):
-        pass
+        root_index = NOTES.index(self.root)
+        notes_in_scale = []
+        for i in self.scale:
+            notes_in_scale.append((root_index + i)%12)
+        notes_in_scale.sort()
+        return notes_in_scale
     
+    def print_row(self, string_note):
+        string_note_index = NOTES.index(string_note)
+        print_out = string_note + "| "
+        for i in range(13):
+            if self.notes_in_scales.count((i + string_note_index) % 12):
+                print_out = print_out + "x "
+            else:
+                print_out = print_out + "- "
+        print(print_out)
+
     def print(self):
-        pass
+        reversed_tuning = self.tuning
+        reversed_tuning.reverse()
+        for string_note in reversed_tuning:
+            self.print_row(string_note)
 
 
 def main():
